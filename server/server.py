9ef731dc73d35e80f80a9cede09604a64d783200
Csaba Pistyur <csaba.pistyur@gmail.com>
2021-06-15 09:33:49 +0200

    Implement the initial server, and fill the requirements.txt

---
+++ server/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['POST', 'GET'])
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=['POST', 'GET'])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/registration", methods=['POST', 'GET'])
+def registration():
+    return render_template("registration.html")
+
+
+@app.route("/userpage", methods=['POST', 'GET'])
+def user_page():
+    return  render_template("userpage.html")
+
+
+@app.route("/game")
+def game():
+    return render_template("game.html")
+
+
