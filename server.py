51ec66ea966f2c76f5d3a54b03a8f274dcdd4100
Totoo95 <juhtam95@gmail.com>
2021-06-15 13:16:43 +0200

    Adding imports to server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 import user_handler
+import util
 
 app = Flask(__name__)
 
