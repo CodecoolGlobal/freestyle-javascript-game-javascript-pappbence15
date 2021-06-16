3ec09327d7af3f43539447d27e8746547e5224cb
Csaba Pistyur <csaba.pistyur@gmail.com>
2021-06-16 15:47:30 +0200

    Implement the array for final images after a spin

--- static/js/game.js
+++ static/js/game.js
@@ -6,6 +6,7 @@ function initGame() {
     const imageNames = ["bar", "cherry", "faszlama", "grapes", "horseshoe", "lemon", "melon", "seven", "strawberry"]
     spinButton.addEventListener("click", manipulate_image);
     function manipulate_image(){
+        spinnedResults = [];
         let images = document.getElementsByClassName("slot_signs");
         for (let i = 0; i <images.length; i++ ) {
             if (i === 0) {
@@ -17,23 +18,32 @@ function initGame() {
                 intervalHandler(i, images, 4000, 7);
             }
         }
+        console.log(spinnedResults);
+
     }
+
     function intervalHandler(pictureIndex, images, stopTime, startingIndex) {
         let j = startingIndex;
-                let slotOneSpin = setInterval(function (){
+        let finalImage;
+        let slotOneSpin = setInterval(function (){
                     images[pictureIndex].src = "static/images/slot_" + imageNames[j]+ ".jpg";
                     j++;
                     if (j===imageNames.length) j=0;
 
                 }, 50);
-                setTimeout(function () {
+        setTimeout(finalImage = function () {
                     let finalPicture = imageNames[Math.floor(Math.random() * imageNames.length)];
                     clearInterval(slotOneSpin);
                     images[pictureIndex].src = "static/images/slot_" + finalPicture+ ".jpg";
+                    console.log("finalpicture:"+finalPicture)
+                    spinnedResults.push(finalPicture);
                 }, stopTime);
     }
 
-
     // Your game can start here, but define separate functions, don't write everything in here :)
+
+
+
+
 
 }
